ac95a8d812527b03b7f38b3f6932b18e84e2dbd4
Hartmut Behrens <hartmut.behrens@gmail.com>
2021-11-30 15:29:38 +0200

    Add AoC 2020 day solution

---
+++ 2020/day01/main.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <ranges>
+
+namespace rng = std::ranges;
+
+[[nodiscard]]
+std::vector<int> read_file(const std::string &filename) {
+    std::ifstream in{filename};
+    // this should make use of RVO
+    std::vector<int> numbers {std::istream_iterator<int>{in},std::istream_iterator<int>{}};
+    return numbers;
+}
+
+int main() {
+    std::vector<int> numbers = read_file("input");
+    // lambda to check if a number exists within the provided input
+    auto has_number = [numbers](int n) {
+        return rng::find(numbers, 2020 - n) != rng::end(numbers);
+    };
+    // part 1 - find two numbers that sum to 2020
+    auto two_numbers = numbers | rng::views::filter(has_number);
+    std::cout << std::accumulate(two_numbers.begin(), two_numbers.end(), 1, std::multiplies<int>()) << "\n";
+    // lambda to check whether any numbers within the provided input, when given a single number from the input
+    // are present in the original input
+    auto is_double_complement = [numbers, &has_number](int x) {
+        return rng::any_of(numbers, [x, &has_number](int y){ return has_number(x+y);});
+    };
+    // part 2 - find three numbers that sum to 2020
+    auto three_numbers = numbers | rng::views::filter(is_double_complement);
+    std::cout << std::accumulate(three_numbers.begin(), three_numbers.end(), 1, std::multiplies<int>());
+}
