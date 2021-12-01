737386dcef0dc3a40018f5f6b7103cd20bdd9d10
Hartmut Behrens <hartmut.behrens@gmail.com>
2021-12-01 21:07:17 +0200

    Generalize algo

--- 2021/day01/main.cpp
+++ 2021/day01/main.cpp
@@ -25,22 +25,26 @@ int main() {
         return delta > 0;
     };
     // part 1 - 1195
-   auto part1_increases = numbers | rng::views::transform(depth_increased);
-   std::cout << "increases (part1) = " << std::accumulate(part1_increases.begin(), part1_increases.end(), 0) << "\n";
-   // part 2
-   int index = 0;
-   auto sliding_depth_increased = [&index, &numbers](int depth) {
-       int delta = 0;
-       if (index > 2) {
-           auto prev_window = rng::views::counted(numbers.begin() + index - 3, 3);
-           auto window = rng::views::counted(numbers.begin() + index - 2, 3);
-           auto prev_depth = std::accumulate(prev_window.begin(), prev_window.end(), 0);
-           auto depth = std::accumulate(window.begin(), window.end(), 0);
-           delta = depth - prev_depth;
-       }
-       index++;
-       return delta > 0;
-   };
+    auto part1_increases = numbers | rng::views::transform(depth_increased);
+    std::cout << "increases (part1) = " << std::accumulate(part1_increases.begin(), part1_increases.end(), 0) << "\n";
+    // part 2
+    int index = 0;
+    int window_size = 3;
+    auto sliding_depth_increased = [&index, &numbers, window_size](int depth) {
+        int delta = 0;
+        int prev_start = index - window_size;
+        int start = index - (window_size - 1);
+        if (index > (window_size - 1)) {
+            auto prev_window = rng::views::counted(numbers.begin() + prev_start, window_size);
+            auto window = rng::views::counted(numbers.begin() + start, window_size);
+            auto prev_depth = std::accumulate(prev_window.begin(), prev_window.end(), 0);
+            auto cur_depth = std::accumulate(window.begin(), window.end(), 0);
+            delta = cur_depth - prev_depth;
+        }
+        index++;
+        return delta > 0;
+    };
+    // part 2 - 1235
     auto part2_increases = numbers | rng::views::transform(sliding_depth_increased);
     std::cout << "increases (part2) = " << std::accumulate(part2_increases.begin(), part2_increases.end(), 0);
 }
