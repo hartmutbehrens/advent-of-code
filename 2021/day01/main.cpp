1a5dad6a66975e953541385c0f51d68874038266
Hartmut Behrens <hartmut.behrens@gmail.com>
2021-12-01 20:48:12 +0200

    Add part 2 solution

--- 2021/day01/main.cpp
+++ 2021/day01/main.cpp
@@ -19,12 +19,28 @@ std::vector<int> read_file(const std::string &filename) {
 int main() {
     std::vector<int> numbers = read_file("input");
     int current_depth = numbers[0];
-    auto did_increase = [&current_depth](int new_depth) {
+    auto depth_increased = [&current_depth](int new_depth) -> bool {
         int delta = new_depth - current_depth;
         current_depth = new_depth;
         return delta > 0;
     };
-    // part 1
-   auto increases = numbers | rng::views::transform(did_increase);
-   std::cout << "increases = " << std::accumulate(increases.begin(), increases.end(), 0);
+    // part 1 - 1195
+   auto part1_increases = numbers | rng::views::transform(depth_increased);
+   std::cout << "increases (part1) = " << std::accumulate(part1_increases.begin(), part1_increases.end(), 0) << "\n";
+   // part 2
+   int index = 0;
+   auto sliding_depth_increased = [&index, &numbers](int depth) {
+       int delta = 0;
+       if (index > 2) {
+           auto prev_window = rng::views::counted(numbers.begin() + index - 3, 3);
+           auto window = rng::views::counted(numbers.begin() + index - 2, 3);
+           auto prev_depth = std::accumulate(prev_window.begin(), prev_window.end(), 0);
+           auto depth = std::accumulate(window.begin(), window.end(), 0);
+           delta = depth - prev_depth;
+       }
+       index++;
+       return delta > 0;
+   };
+    auto part2_increases = numbers | rng::views::transform(sliding_depth_increased);
+    std::cout << "increases (part2) = " << std::accumulate(part2_increases.begin(), part2_increases.end(), 0);
 }
