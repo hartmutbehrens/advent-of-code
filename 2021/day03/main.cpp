6f345a82ee6715be2bad4a32d1bf6135c219e010
Hartmut Behrens <hartmut.behrens@gmail.com>
2021-12-04 13:54:27 +0200

    Add 2021 - day 3 - complete

--- 2021/day03/main.cpp
+++ 2021/day03/main.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
-#include <numeric>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <string>
 #include <vector>
-#include <ranges>
+#include <bitset>
+#include <functional>
 
 namespace rng = std::ranges;
 
@@ -18,24 +18,40 @@ std::vector<std::string> read_file(const std::string &filename) {
 
 int main() {
     std::vector<std::string> diagnostics = read_file("input");
-    std::array<int, 12> one_count{};
-    auto count_ones = [&one_count](const std::string& line) {
-        int i = 0;
-        for (const char c: line) {
-            one_count[i] += c - '0';
-            ++i;
+    std::vector<std::bitset<12>> bitsets(diagnostics.size());
+    std::transform(diagnostics.begin(), diagnostics.end(),
+                   bitsets.begin(), [](const std::string& s) { return std::bitset<12>{s};});
+    std::vector<int> sum_at_each_pos(12);
+    auto count_ones = [&sum_at_each_pos](const auto& bitset){
+        for(int i=0; i<12; ++i) {
+            sum_at_each_pos[i] += bitset[i];
         }
     };
+    rng::for_each(bitsets, count_ones);
+    std::bitset<12> gamma{};
+    for (int i=0; i<12; ++i) {
+        gamma[i] = sum_at_each_pos[i] > 0.5 * diagnostics.size();
+    }
+    // epsilon is just inverse of gamma
+    std::bitset<12> epsilon = ~gamma;
+    // part 1 = 2035764
+    std::cout << "part 1 = " << gamma.to_ulong() * epsilon.to_ulong() << "\n";
 
-    auto to_string = [](auto view) {
-        std::string r;
-        for(auto i: view) {
-            r += std::to_string(i);
+    auto get_rating = [](std::vector<std::bitset<12>> bitsets, std::bitset<12> pattern, auto compare) {
+        int bit_pos = 11; // start at MSB
+        while (bitsets.size() > 1) {
+            int sum_at_pos = 0;
+            for (auto b: bitsets) {
+                sum_at_pos += b[bit_pos];
+            }
+            bool bit_to_match = sum_at_pos >= 0.5 * bitsets.size();
+            std::erase_if(bitsets, [bit_pos, bit_to_match, compare](auto b) { return compare(b[bit_pos], bit_to_match); });
+            --bit_pos;
         }
-        return r;
+        return bitsets[0];
     };
-    rng::for_each(diagnostics, count_ones);
-    auto gamma = one_count | rng::views::transform([diagnostics](int n) { return n > 0.5 * diagnostics.size(); });
-    auto epsilon = one_count | rng::views::transform([diagnostics](int n) { return n < 0.5 * diagnostics.size(); });
-    std::cout << "part 1 = " << std::stoi(to_string(gamma), 0, 2) * std::stoi(to_string(epsilon), 0, 2) << "\n";
+    std::bitset<12> oxygen_rating = get_rating(bitsets, gamma, std::not_equal_to<bool>());
+    std::bitset<12> scrubber_rating = get_rating(bitsets, epsilon, std::equal_to<bool>());
+    // part 2 - 2817661
+    std::cout << "part 2 = " << oxygen_rating.to_ulong() * scrubber_rating.to_ulong() << "\n";
 }
