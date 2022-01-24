496b44b5aaee4cc53eb51b36c9390e5d6a665ac6
Hartmut Behrens <hartmut.behrens@gmail.com>
2022-01-24 21:32:09 +0200

    Add 2021 - day12 - part2

--- 2021/day12/main.cpp
+++ 2021/day12/main.cpp
@@ -27,11 +27,27 @@ auto read_file(const std::string &filename) {
     return caves;
 }
 
+[[nodiscard]]
 bool is_small_cave(const std::string& s) {
     return std::islower(s[0]);
 }
 
-void search(const std::string& cave, auto &cave_system, auto &paths, std::set<std::string> visited = {}, std::vector<std::string> path = {}) {
+[[nodiscard]]
+bool can_visit_small_cave_again(const auto& path) {
+    for (const std::string& s: path) {
+        if (is_small_cave(s) && std::count(path.begin(), path.end(), s) > 1 ) {
+            return false;
+        }
+    }
+    return true;
+};
+
+void search(const std::string& cave,
+            auto &cave_system,
+            auto &paths,
+            bool visit_again = false,
+            std::set<std::string> visited = {},
+            std::vector<std::string> path = {}) {
     path.push_back(cave);
     if (cave == "end") {
         paths.push_back(path);
@@ -40,11 +56,15 @@ void search(const std::string& cave, auto &cave_system, auto &paths, std::set<st
     visited.insert(cave);
     for (const auto &next: cave_system[cave]) {
         if (is_small_cave(next)) {
-            if (!visited.contains(next)) {
-                search(next, cave_system, paths, visited, path);
+            if (visited.contains(next)) {
+                if (visit_again && can_visit_small_cave_again(path) && (next != "start") && (next != "end")) {
+                    search(next, cave_system, paths, visit_again, visited, path);
+                }
+            } else {
+                search(next, cave_system, paths, visit_again, visited, path);
             }
         } else {
-            search(next, cave_system, paths, visited, path);
+            search(next, cave_system, paths, visit_again, visited, path);
         }
     }
 }
@@ -53,7 +73,10 @@ void search(const std::string& cave, auto &cave_system, auto &paths, std::set<st
 int main() {
     auto cave_system = read_file("input");
     std::vector<std::vector<std::string>> paths;
-    search("start", cave_system, paths);
+    search("start", cave_system, paths, false, {}, {});
     std::cout << "number paths = " << paths.size() << "\n";
+    std::vector<std::vector<std::string>> more_paths;
+    search("start", cave_system, more_paths, true, {}, {});
+    std::cout << "number paths with visiting one small cave twice= " << more_paths.size() << "\n";
     return 0;
 }
